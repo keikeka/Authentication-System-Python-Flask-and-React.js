946ce9882c8c5687e4a3e2c7051578c45713f132
keikeka <keilirosalesr@gmail.com>
2022-12-14 11:59:05 +0000

    Modified models routes and app files

--- src/api/routes.py
+++ src/api/routes.py
@@ -1,18 +1,48 @@
-"""
-This module takes care of starting the API Server, Loading the DB and Adding the endpoints
-"""
-from flask import Flask, request, jsonify, url_for, Blueprint
+from flask import Flask, request, jsonify, url_for, Blueprint, json
 from api.models import db, User
 from api.utils import generate_sitemap, APIException
+from flask_jwt_extended import create_access_token, jwt_required, get_jwt_identity
+from datetime import timedelta
 
 api = Blueprint('api', __name__)
 
+# Route Signup 
+@api.route('/signup', methods=['POST'])
+def signup(): 
+    email = request.json.get("email")
+    password = request.json.get("password")  
+    newUser = User(email = email, password = password)
+    db.session.add(newUser)
+    db.session.commit()   
+    return jsonify(newUser.serialize()), 200
 
-@api.route('/hello', methods=['POST', 'GET'])
-def handle_hello():
+# Route Login 
+@api.route('/login', methods=['POST', 'GET']) ## LISTO
+def login():
+    email = request.json.get("email")
+    password = request.json.get("password")
+    user = User.query.filter_by(email=email).first()
+    
+    if user: 
+        if password != user.password: 
+            return jsonify({"message" : "Bad email or password"}), 401
+        else: 
+            time = timedelta(minutes = 30)
+            access_token = create_access_token(identity = user.id, expires_delta = time)
+            return jsonify(access_token = access_token)
+    else:
+        return jsonify({"message" : "Invalid email"}), 401
+    
+# Route Private 
+@api.route('/protected', methods=['GET'])
+@jwt_required() 
+def protected(): 
+    current_id = get_jwt_identity()
+    user = User.query.get(current_id)
+    if user: 
+        return jsonify({"Logged_in" : True}), 200
+    else:
+        return jsonify({"Logged_in" : False}), 400   
 
-    response_body = {
-        "message": "Hello! I'm a message that came from the backend, check the network tab on the google inspector and you will see the GET request"
-    }
 
-    return jsonify(response_body), 200
+
